63bd87074f07a55de28b79989c50f16b085fda4d
hansen9 <79235117+hansen9@users.noreply.github.com>
2024-05-17 09:45:00 +0700

    adding more to profile view and add logo, create view and contents of create view

--- imagine-frontend/src/components/TopBar.vue
+++ imagine-frontend/src/components/TopBar.vue
@@ -2,10 +2,11 @@
     <div class="navbar">
         <div class="float-left d-flex align-content-start flex-wrap">
             <v-img
-            src="@/assets/logo.png"
+            src="@/assets/imagine-logo-zip-file/png/logo-no-background-no-text.png"
             width="70"
+            class="ma-2"
             />
-            <nav>
+            <nav class="ma-3">
                 <router-link to="/">Home</router-link> |
                 <router-link to="/about">About</router-link> | 
                 <router-link to="/explore">Explore</router-link>
@@ -13,12 +14,20 @@
         </div>
         
 
-        <v-card-actions class="float-right">
+        <v-card-actions class="float-right" v-if="!isLogin">
             <v-btn flat class="grey-text" :to="{name: 'login'}">
                 Log In
             </v-btn>
             <v-btn flat class="grey-text" :to="{name: 'signup'}">
                 Sign up
+            </v-btn>
+        </v-card-actions>
+        <v-card-actions class="float-right" v-else>
+            <v-btn flat class="grey-text" :to="{name: 'profile'}">
+                Account
+            </v-btn>
+            <v-btn flat class="grey-text" :to="{name: 'create'}">
+                Create
             </v-btn>
         </v-card-actions>
     </div>
@@ -28,22 +37,26 @@
 <script>
 export default {
     name: 'TopBar',
-    
+    data() {
+    return {
+      "isLogin":true,
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
     .navbar{
-        background-color: #C5FF95;
+        background-color: #7BC9FF;
         height: 100px;
         padding-left: 5px;
         padding-right: 5px;
     }
-    a{
+    /* a{
         color: #5DEBD7;
     }
     a:visited{
         color: #1679AB;
-    }
+    } */
 </style>
---
+++ imagine-frontend/src/views/CreateView.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-card
+    width="720"
+    height="500"
+    class="bg-grey mx-auto mt-10 pa-4"
+    >
+        <h1>this is create page</h1>
+        
+    </v-card>
+</template>
+
+<script>
+export default {
+    name:'CreateView'
+}
+</script>
+
+<style>
+
+</style>
