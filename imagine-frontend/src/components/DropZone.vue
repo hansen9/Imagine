ec2de0a5c4c8ce73f78d2347161e3d23d7337e5b
hansen9 <79235117+hansen9@users.noreply.github.com>
2024-06-19 13:29:58 +0700

    drop zone uploader finished(fronetend wise :))

---
+++ imagine-frontend/src/components/DropZone.vue
@@ -0,0 +1,67 @@
+<template>
+    <div 
+    @dragenter.prevent="toggleActive" 
+    @dragleave.prevent="toggleActive"
+    @dragover.prevent
+    @drop.prevent="toggleActive"
+    :class="{ 'active-dropzone': active }"
+    class="dropzone">
+        <span>Drag or drop files</span>
+        <span>OR</span>
+        <label for="dropzoneFile">select file</label>
+        <input type="file" multiple accept="image/*" id="dropzoneFile" class="dropzoneFile" />
+    </div>
+</template>
+
+<script>
+import {ref} from "vue"
+export default {
+    name: "DropZone",
+    setup(){
+        const active = ref(false)
+
+        const toggleActive= () => {
+            active.value = !active.value
+        }
+
+        return {active, toggleActive}
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    .dropzone{
+        margin: auto;
+        width: 400px;
+        height: 200px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        row-gap: 16px;
+        border: 2px dashed green;
+        background-color: aliceblue;
+        transition: 0.3s ease all;
+
+        label{
+            padding: 8px 12px;
+            color: green;
+            transition: 0.3s ease all;
+        }
+
+        input{
+            display: none;
+        }
+    }
+
+    .active-dropzone{
+        color: beige;
+        border-color: beige;
+        background-color: bisque;
+
+        label{
+            background-color: antiquewhite;
+            color: aqua;
+        }
+    }
+</style>
--- imagine-frontend/src/views/CreateView.vue
+++ imagine-frontend/src/views/CreateView.vue
@@ -2,19 +2,46 @@
     <v-card
     width="720"
     height="500"
-    class="bg-grey mx-auto mt-10 pa-4"
+    class="bg-grey ma-auto mt-10 pa-4"
     >
-        <h1>this is create page</h1>
-        
+        <div class="ma-auto"> 
+            <h1>this is create page</h1>
+            <DropZone @drop.prevent="drop" @change="selectedFile" />
+            <img 
+            class="file-image ma-auto" 
+            :src="imageURL"
+            width="700"
+            height="200"
+            >
+            <span class="file-name">File: {{ dropzoneFile.name }}</span>
+        </div>
     </v-card>
 </template>
 
 <script>
+import DropZone from '@/components/DropZone.vue'
+import {ref} from "vue"
+
 export default {
-    name:'CreateView'
+    name:'CreateView',
+    components:{
+        DropZone,
+    },
+    setup() {
+        let dropzoneFile = ref("");
+        let imageURL = ref(null)
+
+        const drop = (event) => {
+            dropzoneFile.value = event.dataTransfer.files[0]
+            imageURL.value = URL.createObjectURL(dropzoneFile.value)
+            console.log(imageURL.value)
+        }
+
+        const selectedFile = () => {
+            dropzoneFile.value = document.querySelector('.dropzoneFile').files
+        }
+
+        return {dropzoneFile, drop, selectedFile, imageURL};
+    },
 }
 </script>
-
-<style>
-
-</style>
